3f51b9361e2c04a60446aca15e8c928e1134f06e
vipasyan <103524280+Vipasyan33@users.noreply.github.com>
2022-04-16 19:14:34 +0200

    Add changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+// var username= prompt("Your name");
+// document.write("Hello "+ username);
+
+var myname = "Vipasyan Telaprolu";
+document.write("Made By "+ myname);
+
 let emailElement = document.querySelector('#email');
 let messageElement= document.querySelector('#message');
 let submitButton = document.querySelector('#submit-button');
